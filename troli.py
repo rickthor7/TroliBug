ac2dff6327b6ed8b49196923acf030e309a261be
rickthor7 <96973185+rickthor7@users.noreply.github.com>
2022-01-03 20:17:32 +0700

    Create troli.py

---
+++ troli.py
@@ -0,0 +1,18 @@
+//FAKEREPLY TROLI
+const ftroli = {
+	key : {
+                          participant : '0@s.whatsapp.net'
+                        },
+       message: {
+                    orderMessage: {
+                            itemCount : 1,
+                            status: 1,
+                            surface : 1,
+                            message: 'YogiPw', //Rickthor7
+                            orderTitle: 'Bang',
+                            thumbnail: fs.readFileSync('./stik/thumb.jpeg'), //Gambarnye
+                            sellerJid: '0@s.whatsapp.net'
+          
+                          }
+                        }
+                      }
